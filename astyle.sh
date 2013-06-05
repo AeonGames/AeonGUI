fa1daaf580d9115cd03b942c4ce61b3fdf705486
Rodrigo Hernandez Cordoba <kwizatz@aeongames.com>
2013-06-05 17:15:31 -0600

    Moved PCX class to a helper library to be used as the default supported file format for images

--- astyle.sh
+++ astyle.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-astyle --options=astylerc "demos/WindowsOpenGL/*.cpp" "demos/LinuxOpenGL/*.cpp" "include/*.h" "renderers/OpenGL/*.cpp" "renderers\OpenGL\*.h"
+astyle --options=astylerc "demos/WindowsOpenGL/*.cpp" "demos/LinuxOpenGL/*.cpp" "include/*.h" "renderers/OpenGL/*.cpp" "renderers/OpenGL/*.h" "common/pcx/*.h" "common/pcx/*.cpp"
