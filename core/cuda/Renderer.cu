66cf312e365ba39e0f271fb9b2f96885b8e44ec5
Rodrigo Hernandez <kwizatz@aeongames.com>
2013-07-17 11:39:56 -0600

    CUDA DrawSubImage for 1/1 ratio on width and height works but is not yet optimized

--- core/cuda/Renderer.cu
+++ core/cuda/Renderer.cu
@@ -1,18 +1,46 @@
-#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
+#include <math_constants.h>
+#include <math_functions.h>
 #include "Renderer.h"
 namespace AeonGUI
 {
     __global__ void blend ( Color* src, size_t src_pitch, uint32_t src_width, uint32_t src_height, Color* dst, size_t dst_pitch, uint32_t dst_width, uint32_t dst_height )
     {
-        // This is just copying over pixel values for now
-        Color* dst_color = ( Color* ) ( ( char* ) dst + threadIdx.y * dst_pitch ) + threadIdx.x;
-        Color* src_color = ( Color* ) ( ( char* ) src + threadIdx.y * src_pitch ) + threadIdx.x;
-        dst_color->a = src_color->a;
-        dst_color->b = src_color->b;
-        dst_color->g = src_color->g;
-        dst_color->r = src_color->r;
+        int32_t x = ( blockIdx.x * blockDim.x ) + threadIdx.x;
+        int32_t y = ( blockIdx.y * blockDim.y ) + threadIdx.y;
+        // Avoid accessing out of bounds memory.
+        if ( ( x > src_width ) || ( y > src_height ) )
+        {
+            return;
+        }
+
+        // Read values into local variables
+        Color dst_color = * ( ( Color* ) ( ( char* ) dst + y * dst_pitch ) + x );
+        Color src_color = * ( ( Color* ) ( ( char* ) src + y * src_pitch ) + x );
+
+        if ( ( src_color.a == 255 ) )
+        {
+            /*  Full source opacity
+                do a simple replacement*/
+            dst_color.bgra = src_color.bgra;
+        }
+        /*  If the source alpha is 0
+            the destination color is unchanged */
+        else if ( src_color.a > 0 )
+        {
+            float sfactor = ( static_cast<float> ( src_color.a ) / 255.0f );
+            float dfactor = 1.0f - sfactor;
+            dst_color.r = static_cast<uint8_t> ( min ( 255.0f, ( static_cast<float> ( src_color.r ) * sfactor + static_cast<float> ( dst_color.r ) * dfactor ) ) );
+            dst_color.g = static_cast<uint8_t> ( min ( 255.0f, ( static_cast<float> ( src_color.g ) * sfactor + static_cast<float> ( dst_color.g ) * dfactor ) ) );
+            dst_color.b = static_cast<uint8_t> ( min ( 255.0f, ( static_cast<float> ( src_color.b ) * sfactor + static_cast<float> ( dst_color.b ) * dfactor ) ) );
+            // Just acumulate alpha, if something looks odd try multipling the second addend by dfactor.
+            dst_color.a = static_cast<uint8_t> ( min ( 255.0f, ( static_cast<float> ( src_color.a ) + static_cast<float> ( dst_color.a ) ) ) );
+        }
+        // Write back to destination
+        * ( ( Color* ) ( ( char* ) dst + y * dst_pitch ) + x ) = dst_color;
     }
 
     void Renderer::DrawSubImage ( Image* image, int32_t x, int32_t y, int32_t subx, int32_t suby, int32_t subw, int32_t subh, int32_t w, int32_t h, ResizeAlgorithm algorithm )
@@ -64,9 +92,10 @@ namespace AeonGUI
             cudaMemcpy2D ( d_screen_rect, d_screen_pitch, pixels + ( ( y * screen_w ) + x ), screen_w * sizeof ( Color ), w * sizeof ( Color ), h, cudaMemcpyHostToDevice );
             cudaMallocPitch ( &d_image_rect, &d_image_pitch, image_w * sizeof ( Color ), image_h );
             cudaMemcpy2D ( d_image_rect, d_image_pitch, image_bitmap, image_w * sizeof ( Color ), image_w * sizeof ( Color ), image_h, cudaMemcpyHostToDevice );
-            ///\todo modify to use only 1024 threads per block.
-            blend <<< 1, dim3 ( w, h ) >>> ( d_image_rect, d_image_pitch, image_w, image_h,
-                                             d_screen_rect, d_screen_pitch, w, h );
+
+            dim3 gridSize ( static_cast<uint32_t> ( ceilf ( static_cast<float> ( w ) / 16.0f ) ), static_cast<uint32_t> ( ceilf ( static_cast<float> ( h ) / 16.0f ) ) );
+            blend <<< gridSize, dim3 ( 16, 16 ) >>> ( d_image_rect, d_image_pitch, image_w, image_h,
+                    d_screen_rect, d_screen_pitch, w, h );
             cudaDeviceSynchronize();
             cudaError_t  code = cudaGetLastError();
             if ( code != cudaSuccess )
