04c7edd3ad0aa7738bc3aeebd9bf1d842024f46c
Rodrigo Hernandez <kwizatz@aeongames.com>
2015-09-30 17:37:43 -0600

    Moved the gitclone function to the functions.cmake utilitary module.

---
+++ cmake/functions.cmake
@@ -0,0 +1,11 @@
+# Copyright 2015 AeonGames, Rodrigo Hernandez
+# Licensed under the terms of the Apache 2.0 License.
+function(gitclone repo path)
+	if(NOT IS_DIRECTORY "${CMAKE_SOURCE_DIR}/${path}")
+		message(STATUS "Cloning ${repo}, please wait")
+		execute_process(COMMAND ${GIT_EXECUTABLE} clone ${repo} ${path} WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}" RESULT_VARIABLE git_result OUTPUT_VARIABLE git_output ERROR_VARIABLE git_output)
+		if(NOT git_result EQUAL 0)
+			MESSAGE(FATAL_ERROR "Cloning ${repo} failed.\nResult: ${git_result}\nOutput: ${git_output}")
+		endif(NOT git_result EQUAL 0)
+	endif(NOT IS_DIRECTORY "${CMAKE_SOURCE_DIR}/${path}")
+endfunction(gitclone repo path)
