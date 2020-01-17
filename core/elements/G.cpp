75cb2c3de2619277e7d7fedda92beafb19164dac
Rodrigo Hernandez <kwizatz@aeongames.com>
2020-01-16 19:15:10 -0600

    Work on JavaScript/SVG interaction.

--- core/elements/G.cpp
+++ core/elements/G.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2019 Rodrigo Jose Hernandez Cordoba
+Copyright (C) 2019,2020 Rodrigo Jose Hernandez Cordoba
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -43,6 +43,14 @@ namespace AeonGUI
                 else if ( i.first == "stroke-width" )
                 {
                     aCanvas.SetStrokeWidth ( std::get<double> ( i.second ) );
+                }
+                else if ( i.first == "stroke-opacity" )
+                {
+                    aCanvas.SetStrokeOpacity ( std::get<double> ( i.second ) );
+                }
+                else if ( i.first == "fill-opacity" )
+                {
+                    aCanvas.SetFillOpacity ( std::get<double> ( i.second ) );
                 }
             }
         }
