ecce54063cef9b955c4b057ba1a54dde5e7cd4fd
Rodrigo Hernandez <kwizatz@aeongames.com>
2023-06-21 00:02:56 -0600

    Added scripts to retrieve and build libCSS from NetSurf

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -1,13 +1,23 @@
-# Copyright 2015 AeonGames, Rodrigo Hernandez
+# Copyright 2015,2023 AeonGames, Rodrigo Hernandez
 # Licensed under the terms of the Apache 2.0 License.
-function(gitclone repo path)
-	if(NOT IS_DIRECTORY "${CMAKE_SOURCE_DIR}/${path}")
+function(gitclone repo path tag)
+	if(NOT IS_DIRECTORY "${path}")
 		message(STATUS "Cloning ${repo}, please wait")
-		execute_process(COMMAND ${GIT_EXECUTABLE} clone ${repo} ${path} WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}" RESULT_VARIABLE git_result OUTPUT_VARIABLE git_output ERROR_VARIABLE git_output)
+		execute_process(COMMAND ${GIT_EXECUTABLE} clone --depth 1 ${repo} ${path} WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}" RESULT_VARIABLE git_result OUTPUT_VARIABLE git_output ERROR_VARIABLE git_output)
 		if(NOT git_result EQUAL 0)
 			MESSAGE(FATAL_ERROR "Cloning ${repo} failed.\nResult: ${git_result}\nOutput: ${git_output}")
 		endif(NOT git_result EQUAL 0)
-	endif(NOT IS_DIRECTORY "${CMAKE_SOURCE_DIR}/${path}")
+
+		execute_process(COMMAND ${GIT_EXECUTABLE} fetch --depth 1 origin ${tag} WORKING_DIRECTORY "${path}" RESULT_VARIABLE git_result OUTPUT_VARIABLE git_output ERROR_VARIABLE git_output)
+		if(NOT git_result EQUAL 0)
+			MESSAGE(FATAL_ERROR "Fetching ${tag} failed.\nResult: ${git_result}\nOutput: ${git_output}")
+		endif(NOT git_result EQUAL 0)
+
+		execute_process(COMMAND ${GIT_EXECUTABLE} checkout FETCH_HEAD WORKING_DIRECTORY "${path}" RESULT_VARIABLE git_result OUTPUT_VARIABLE git_output ERROR_VARIABLE git_output)
+		if(NOT git_result EQUAL 0)
+			MESSAGE(FATAL_ERROR "Checkout ${FETCH_HEAD} failed.\nResult: ${git_result}\nOutput: ${git_output}")
+		endif(NOT git_result EQUAL 0)
+	endif(NOT IS_DIRECTORY "${path}")
 endfunction(gitclone repo path)
 
 function(download url filename)
