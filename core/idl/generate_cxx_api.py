1f60ae102a2e32576f3968ff3ae8fb91c6e590ed
Rodrigo Hernandez <kwizatz@aeongames.com>
2025-05-24 16:49:06 -0600

    Added code to write constructors and initial attributes on IDL headers.

--- core/idl/generate_cxx_api.py
+++ core/idl/generate_cxx_api.py
@@ -36,20 +36,25 @@ limitations under the License.
 
 IDL_TYPES = ['DOMString','boolean']
 
-def write_header(filename, idl, includes, members):
+def write_header(filename, idl, includes, public, private):
             header = open(filename, 'wt')
             header.write(license)
-            header.write("#ifndef AEONGUI_{0}_H\n".format(idl['name'].upper()))
-            header.write("#define AEONGUI_{0}_H\n".format(idl['name'].upper()))
+            header.write("#ifndef AEONGUI_DOM_{0}_H\n".format(idl['name'].upper()))
+            header.write("#define AEONGUI_DOM_{0}_H\n".format(idl['name'].upper()))
             for j in includes:
                 header.write("#include \"{0}\"\n".format(j))
             header.write("namespace AeonGUI\n{\n\tnamespace DOM\n\t{\n".expandtabs(4))
-            header.write("\t\t{} {} {}\n\t\t{{{}\n".expandtabs(4).format("class" if idl['type'] == 'interface' else "struct", idl['name'], '' if idl['inheritance'] == None else ": public {}".format(idl['inheritance']), "\n\t\tpublic:" if idl['type'] == 'interface' else "" ))
-            header.write("\t\t\t{};\n".format("\n\t\t\t".join(members)).expandtabs(4))
+            header.write("\t\t{} {} {}\n\t\t{{\n".expandtabs(4).format("class" if idl['type'] == 'interface' else "struct", idl['name'], '' if idl['inheritance'] == None else ": public {}".format(idl['inheritance'])))
+            if len(public) != 0:
+                header.write("\t\tpublic:\n".expandtabs(4))
+                header.write("\t\t\t{}\n".format("\n\t\t\t".join(public)).expandtabs(4))
+            if len(private) != 0:
+                header.write("\t\tprivate:\n".expandtabs(4))
+                header.write("\t\t\t{}\n".format("\n\t\t\t".join(private)).expandtabs(4))
             header.write("\t\t};\n".expandtabs(4))
             header.write("\t}\n".expandtabs(4))
             header.write("}\n".expandtabs(4))
-            header.write("#endif // AEONGUI_{0}_H\n".format(idl['name'].upper()))
+            header.write("#endif // AEONGUI_DOM_{0}_H\n".format(idl['name'].upper()))
             header.close()
 
 def main():
@@ -71,25 +76,22 @@ def main():
         idl_names_to_process = {args.interface: idl['idlNames'][args.interface]}
     
     for i in idl_names_to_process:
-        if idl_names_to_process[i]['type'] == 'dictionary':
-            includes = []
-            members = []
-            for member in idl_names_to_process[i]['members']:
-                if member['type'] == 'field':
-                    if member['idlType']['idlType'] in IDL_TYPES:
-                        if 'WebIDL_Types.h' not in includes:
-                            includes.append('WebIDL_Types.h')
-                    else:
-                        if member['idlType']['idlType'] not in includes:
-                            includes.append("{0}.h".format(member['idlType']['idlType']))
-                    members.append("{0} {1}{2};".format(member['idlType']['idlType'], member['name'], "" if member['default'] == None or 'value' not in member['default'] else "{{{}}}".format(str(member['default']['value']).lower() if 'type' in member['default'] and member['default']['type'] == 'boolean' else member['default']['value'])))
-            write_header("{0}/{1}.h".format(args.output_dir,i),idl_names_to_process[i],includes,members)
-        elif idl_names_to_process[i]['type'] == 'interface':
-            includes = []
-            members = []
+        includes = []
+        public = []
+        private = []
+        if idl_names_to_process[i]['type'] == 'interface':
             if idl_names_to_process[i]['inheritance'] != None:
                 includes.append('{0}.h'.format(idl_names_to_process[i]['inheritance']))
+            constructor = f"{idl_names_to_process[i]['name']}();"
+            destructor = f"~{idl_names_to_process[i]['name']}();"
             for member in idl_names_to_process[i]['members']:
+                if member['idlType']['idlType'] in IDL_TYPES:
+                    if 'WebIDL_Types.h' not in includes:
+                        includes.append('WebIDL_Types.h')
+                else:
+                    if "{0}.h".format(member['idlType']['idlType']) not in includes:
+                        includes.append("{0}.h".format(member['idlType']['idlType']))
+
                 if member['type'] == 'constructor':
                     arguments = []
                     for argument in member['arguments']:
@@ -100,7 +102,11 @@ def main():
                             if argument['idlType']['idlType'] not in includes:
                                 includes.append("{0}.h".format(argument['idlType']['idlType']))
                         arguments.append("{0} {1}{2}".format(argument['idlType']['idlType'], argument['name'], " = {}" if argument['optional'] else "" ))
-                    members.append("{0}({1})".format(i,", ".join(arguments)))
-            write_header("{0}/{1}.h".format(args.output_dir,i),idl_names_to_process[i],includes,members)
+                    constructor = "{0}({1})".format(idl_names_to_process[i]['name'],", ".join(arguments))
+                elif member['type'] == 'attribute':
+                    public.append("{0} {1};".format(member['idlType']['idlType'], member['name']))
+            public.append(constructor)
+            public.append(destructor)
+        write_header("{0}/{1}.h".format(args.output_dir,i),idl_names_to_process[i],includes,public,private)
 if __name__ == "__main__":
     main()
