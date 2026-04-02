617e7866b23cec48a134468af204a452cae0fa08
Rodrigo Hernandez <kwizatz@aeongames.com>
2026-04-02 14:22:12 -0600

    Normalize line endings to LF

--- tools/generate_class.py
+++ tools/generate_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Copyright (C) 2025 Rodrigo Jose Hernandez Cordoba
+Copyright (C) 2025,2026 Rodrigo Jose Hernandez Cordoba
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
